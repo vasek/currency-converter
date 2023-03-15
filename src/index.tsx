e2d7959d820b6ffedd31bb93882d0458f8be4b19
Václav Ostrožlík <vaclav@ostrozlik.cz>
2023-03-15 13:51:41 +0100

    Use `createRoot` method to make the app compatible with React 18

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,17 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "react-query";
 import App from "./App";
 
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
 const queryClient = new QueryClient();
 
-ReactDOM.render(
+root.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
     </QueryClientProvider>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
